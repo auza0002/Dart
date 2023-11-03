26d105be5badd913611c7e80d1fb6e43b9d42c1f
Diego Auza <auza0002@algonquinlive.com>
2023-11-03 16:42:54 -0400

    Dart one: created folder

---
+++ first_console_full_project/bin/first_console_full_project.dart
@@ -0,0 +1,5 @@
+import 'package:first_console_full_project/first_console_full_project.dart' as first_console_full_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_console_full_project.calculate()}!');
+}
---
+++ first_console_full_project/test/first_console_full_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_console_full_project/first_console_full_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
