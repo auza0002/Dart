58e5e498d53fa14236afa4452a3aa1c6605b5176
Diego Auza <auza0002@algonquinlive.com>
2023-11-03 17:11:18 -0400

    Data type

--- first_console_full_project/bin/first_console_full_project.dart
+++ first_console_full_project/bin/first_console_full_project.dart
@@ -1,5 +1,41 @@
-import 'package:first_console_full_project/first_console_full_project.dart' as first_console_full_project;
-
 void main(List<String> arguments) {
-  print('Hello world: ${first_console_full_project.calculate()}!');
+  // variable
+
+  // String
+  String nameUser = "Diego";
+  // Integer
+  int x = 41;
+  //  Dynamic
+  dynamic firstName = "Tim";
+
+  // Const, final
+  const String fullName = "Diego Auza";
+  final String nickName =
+      "J"; // if the data is going to change we use final on a run
+
+  // declaring and not using
+
+  var myName;
+  print(myName);
+  print(firstName);
+  print(x);
+  print(nameUser);
+
+  // Data type
+  // Numbers, Strings , Booleans, Lists, Map, Dynamic
+  // Strings
+  String firstNameData = "Diego";
+  print("String $firstNameData");
+  // Integers
+  int myNumber = 35;
+  print("My number is: $myNumber");
+  // Double
+  double otherNumberDouble = 19.95;
+  print(otherNumberDouble);
+  // Booleans
+  bool myBool = false;
+  print("This is a boolean: $myBool");
+  //Dynamic
+  dynamic fullNameDynamic = "Jhon Elder";
+  print(fullNameDynamic);
 }
